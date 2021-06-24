10b2e7ab8c8c44085edfd7ce2e88c898aafdbd55
bmp-ntnx <bmpeterson@gmail.com>
2021-06-23 21:53:14 -0600

    Update launchbp.ps1

--- launchbp.ps1
+++ launchbp.ps1
@@ -1,30 +1,45 @@
-$Uri = "https://sa.lab.local:9440/api/nutanix/v3/blueprints/12fc21e7-4362-4a77-917c-10c5914867d0/simple_launch"
-$username = "xxxx"
-$password = "xxxx"
+#Payload variables
+$sysname = "ChangeME10"
+$cpu = 8 
+
+#UUID variables
+$bpuuid = "12fc21e7-4362-4a77-917c-10c5914867d0"
+$appuuid = "abe58561-5745-40f1-aef1-29c593ac5f15"
+$substrateuuid = "5214fc2d-7a8f-476a-a480-c51be863cb2b"
+
+
+#Connection variables 
+$pc = "sa.lab.local"
+$username = "xxxxx"
+$password = "xxxxx"
+
+#Setup API call
+$uri = "https://"+$pc+":9440/api/nutanix/v3/blueprints/"+$bpuuid+"/simple_launch"
 $Header = @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($username+":"+$password ))}
 $ContentType = "application/json"
-Invoke-RestMethod -SkipCertificateCheck -Uri $Uri -ContentType $ContentType -Headers $Header -Body $Body -Method "POST"
 
-$Body = @'
+#API Call to Calm
+Invoke-RestMethod -SkipCertificateCheck -Uri $uri -ContentType $ContentType -Headers $Header -Body $Body -Method "POST"
+
+#JSON Payload
+$Body = '
 {
     "spec": {
-        "app_name": "CHANGEME2",
+        "app_name": "'+$sysname+'",
         "app_description": "REST API",
         "app_profile_reference": {
-            "kind": "app_profile",
-            "name": "AHV",
-            "uuid": "abe58561-5745-40f1-aef1-29c593ac5f15"
+            "uuid": "'+$appuuid+'"
         },
         "runtime_editables": {
             "substrate_list": [
               {
                 
-                "uuid": "5214fc2d-7a8f-476a-a480-c51be863cb2b",
+                "uuid": "'+$substrateuuid+'",
                 "value": {
                   "spec": {
-                    "name": "CHANGEME2",
+                    "name": "'+$sysname+'",
                      "resources": {
-                      "num_sockets": 4
+                      "num_sockets": '+$cpu+'
                     }
                   }
                 }
@@ -33,4 +48,4 @@ $Body = @'
           }
     }
 }
-'@
+'
